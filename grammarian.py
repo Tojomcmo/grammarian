14db6fba5bf0035ac9773e74471a0fd46b0d4ff9
tojomcmo <tojomcmo@gmail.com>
2020-05-16 01:02:33 -0400

    grammarian search code for substitutions deletions and shifts, first commit

--- grammarian.py
+++ grammarian.py
@@ -4,34 +4,129 @@ import string
 d = enchant.Dict("en_US")
 ALPHABET = string.ascii_lowercase
 
-# list of phrases
-# break into individual words
-# Iterate each letter A to Z
-# check if resultant word is english
-# if true and not original - store
+# ASSUMPTIONS:
+#   The grammarian orb allows for the user to "change one letter" in the spell name
+#   Changing is assumed to take three forms:
+#       - altering the value
+#       - moving the location
+#       - removing
+#   Adding a new letter is changing the phrase, but not changing an existing letter, and is therefore out of scope
+#   from a strict reading of the action. This is an opinion. Fight me.
+#   Spaces and word count are also assumed constant, though this is less fight-worthy, could be swayed to include.
 
-def grammarize_word(word):
+#   The function grammarize_phrase receives a phrase (letters and spaces only) and returns a list of altered phrases,
+#   where each returned phrase complies with one of the above forms, and each word in the phrase complies with the
+#   PyEnchant english dictionary.
+
+# FUNCTION LIST:
+#   grammarize_word_substitute - returns a list of words from substituting one letter of the input word with
+# another letter from the alphabet
+#   grammarize_word_delete - returns a list of words from deleting one letter from the input word
+#   grammarize_phrase_shift - returns a list of phrases from shifting one letter across the input phrase
+#   grammarize_phrase_substitute - returns a list of phrases using the word substitute function to an input phrase
+#   grammarize_phrase_deletion - returns a list of phrases using the word delete function to an input phrase
+#   grammarize_phrase - employs all three phrase functions to deliver the conglomerated set of grammarized phrases
+# created from the input phrase
+
+
+def grammarize_word_substitute(word):
     words = []
+    # this for loop targets each indexed letter of the input word
     for i in range(len(word)):
+        # this for loop substitutes the target letter the alphabet
         for letter in ALPHABET:
             grammarized_word = word[:i] + letter + word[i + 1:]
+            # this if statement checks the new word against the dictionary and the input word
             if d.check(grammarized_word) and grammarized_word != word:
                 words.append(grammarized_word)
     return words
 
-def grammarize_phrase(phrase):
+
+def grammarize_word_delete(word):
+    words = []
+    # this for loop targets each indexed letter of the input word and deletes the letter
+    for i in range(len(word)):
+        grammarized_word = word[:i] + word[i + 1:]
+        # this if statement checks the new word against the dictionary and the input word
+        if d.check(grammarized_word) and grammarized_word != word:
+            words.append(grammarized_word)
+    return words
+
+def grammarize_phrase_shift(phrase):
+    grammarized_phrases_shift_set = []
+    # this for loop selects the letter to shift and creates the phrase with the target letter removed
+    for i in range(len(phrase)):
+        target_letter = phrase[i]
+        target_phrase = phrase[:i] + phrase[i + 1:]
+        # This if statement rejects a shift of spaces
+        if target_letter != " ":
+            # this for loop shifts the target letter through the altered phrase
+            for j in range(len(target_phrase)+1):
+                grammarized_phrase =target_phrase[:j] + target_letter + target_phrase[j:]
+                phrase_words = grammarized_phrase.split()
+                counter = 0
+                # this for loop checks whether all individual words in the target configuration are in the dictionary
+                for k in range(len(phrase_words)):
+                    if d.check(phrase_words[k]):
+                        counter += 1
+                # if each word is in the dictionary, then the phrase is viable
+                if counter is len(phrase_words) and grammarized_phrase != phrase:
+                    grammarized_phrases_shift_set.append(grammarized_phrase)
+    return grammarized_phrases_shift_set
+
+def grammarize_phrase_substitute(phrase):
+    # the split operation separates the phrase into a list of individual words
     phrase_words = phrase.split()
-    grammarized_phrases = []
+    grammarized_phrases_substitute_set = []
+    # this for loop iterates the word targeted by the grammarize word substitute function
     for i in range(len(phrase_words)):
         target_word = phrase_words[i]
-        grammarized_word = grammarize_word(target_word)
+        grammarized_word = grammarize_word_substitute(target_word)
+        # this for loop incorporates list of successes for the targeted word adds them to the phrase
         for j in range(len(grammarized_word)):
-            grammarized_phrase = phrase.split()
-            grammarized_phrase[i] = grammarized_word[j]
+            target_grammarized_phrase = phrase.split()
+            target_grammarized_phrase[i] = grammarized_word[j]
             space = " "
-            grammarized_phrase = space.join(grammarized_phrase)
-            grammarized_phrases.append(grammarized_phrase)
-    return grammarized_phrases
+            target_grammarized_phrase = space.join(target_grammarized_phrase)
+            # option added to list of successes
+            grammarized_phrases_substitute_set.append(target_grammarized_phrase)
+    return grammarized_phrases_substitute_set
 
 
-print(grammarize_phrase("beacon of hope"))
+def grammarize_phrase_delete(phrase):
+    # the split operation separates the phrase into a list of individual words
+    phrase_words = phrase.split()
+    grammarized_phrases_delete_set = []
+    # this for loop iterates the word targeted by the grammarize word substitute function
+    for i in range(len(phrase_words)):
+        target_word = phrase_words[i]
+        grammarized_word = grammarize_word_delete(target_word)
+        # this for loop incorporates list of successes for the targeted word adds them to the phrase
+        for j in range(len(grammarized_word)):
+            target_grammarized_phrase = phrase.split()
+            target_grammarized_phrase[i] = grammarized_word[j]
+            space = " "
+            target_grammarized_phrase = space.join(target_grammarized_phrase)
+            # option added to list of successes
+            grammarized_phrases_delete_set.append(target_grammarized_phrase)
+    return grammarized_phrases_delete_set
+
+def grammarize_phrase(phrase):
+    grammarized_phrase_set = []
+    substitute = grammarize_phrase_substitute(phrase)
+    delete = grammarize_phrase_delete(phrase)
+    shift = grammarize_phrase_shift(phrase)
+    for i in range(len(substitute)):
+        grammarized_phrase_set.append(substitute[i])
+    for j in range(len(delete)):
+        grammarized_phrase_set.append(delete[j])
+    for k in range(len(shift)):
+        grammarized_phrase_set.append(shift[k])
+
+    return grammarized_phrase_set
+
+
+
+# list = grammarize_phrase("spare the dying")
+# for i in list:
+#     print(i)
--- grammarian_test.py
+++ grammarian_test.py
@@ -3,35 +3,131 @@ import grammarian as gm
 
 
 class MyTestCase(unittest.TestCase):
-    def test_grammarize_word_generates_word_with_substitution(self):
-        words = gm.grammarize_word('fear')
+
+    #
+    #
+    # grammarize by substituting one letter
+    def test_grammarize_word_substitute_generates_word_with_substitution(self):
+        words = gm.grammarize_word_substitute('fear')
         self.assertIn('bear', words)
         self.assertIn('feat', words)
 
-    def test_grammarize_word_does_not_include_original_word(self):
-        words = gm.grammarize_word('fear')
+    def test_grammarize_word_substitute_does_not_include_original_word(self):
+        words = gm.grammarize_word_substitute('fear')
         self.assertNotIn('fear', words)
 
-    def test_grammarize_word_does_not_include_word_with_multiple_substitutions(self):
-        words = gm.grammarize_word('fear')
+    def test_grammarize_word_does_substitute_not_include_word_with_multiple_substitutions(self):
+        words = gm.grammarize_word_substitute('fear')
         self.assertNotIn('beer', words)
 
-    def test_grammarize_phrase_generates_phrase_with_substitution(self):
-        words = gm.grammarize_phrase('make fear')
-        self.assertIn('bake fear', words)
-        self.assertIn('make bear', words)
-        self.assertIn('make feat', words)
-        self.assertIn('made fear', words)
+    #
+    #
+    # grammarized letter removal in word
+    def test_grammarize_word_delete_generates_word_with_removal(self):
+        words = gm.grammarize_word_delete('fear')
+        self.assertIn('far', words)
+        self.assertIn('ear', words)
 
-    def test_grammarize_phrase_does_not_include_original_phrase(self):
-        words = gm.grammarize_phrase('make fear')
-        self.assertNotIn('make fear', words)
-        self.assertNotIn('bake bear', words)
+    def test_grammarize_word_delete_does_not_include_original_word(self):
+        words = gm.grammarize_word_delete('fear')
+        self.assertNotIn('fear', words)
 
-    def test_grammarize_phrase_does_not_include_multiple_words_with_substitutions(self):
-        words = gm.grammarize_phrase('make fear')
-        self.assertNotIn('bake bear', words)
+    def test_grammarize_word_delete_does_not_include_word_with_multiple_removals(self):
+        words = gm.grammarize_word_delete('fear')
+        self.assertNotIn('a', words)
 
+    #
+    #
+    # phrases from letter substitution
+
+    def test_grammarize_phrase_substitute_generates_phrase_with_substitution(self):
+        phrases = gm.grammarize_phrase_substitute('make fear')
+        self.assertIn('bake fear', phrases)
+        self.assertIn('make bear', phrases)
+        self.assertIn('make feat', phrases)
+        self.assertIn('made fear', phrases)
+
+    def test_grammarize_phrase_substitute_does_not_include_original_phrase(self):
+        phrases = gm.grammarize_phrase_substitute('make fear')
+        self.assertNotIn('make fear', phrases)
+        self.assertNotIn('bake bear', phrases)
+
+    def test_grammarize_phrase_substitute_does_not_include_multiple_words_with_substitutions(self):
+        phrases = gm.grammarize_phrase_substitute('make fear')
+        self.assertNotIn('bake bear', phrases)
+
+    #
+    #
+    # phrases from letter removal
+
+    def test_grammarize_phrase_delete_generates_phrases_with_deletions(self):
+        phrases = gm.grammarize_phrase_delete('fear fare')
+        self.assertIn('far fare', phrases)
+        self.assertIn('fear far', phrases)
+
+    def test_grammarize_phrase_delete_includes_words_with_no_deletions(self):
+        phrases = gm.grammarize_phrase_delete('make fear fare')
+        self.assertIn('make far fare', phrases)
+        self.assertIn('make ear fare', phrases)
+
+    def test_grammarize_word_delete_does_not_include_original_word(self):
+        phrases = gm.grammarize_phrase_delete('fear fare')
+        self.assertNotIn('fear fare', phrases)
+
+    def test_grammarize_word_delete_does_not_include_word_with_multiple_removals(self):
+        phrases = gm.grammarize_phrase_delete('fear fare')
+        self.assertNotIn('ear far', phrases)
+
+    #
+    #
+    # phrases from letter shifting
+    def test_grammarize_phrase_shift_generates_phrase_with_letter_shift(self):
+        phrases = gm.grammarize_phrase_shift('make fear')
+        self.assertIn('make fare', phrases)
+
+    def test_grammarize_phrase_shift_does_not_include_original_phrase(self):
+        phrases = gm.grammarize_phrase_shift('make fear')
+        self.assertNotIn('make fear', phrases)
+
+    def test_grammarize_phrase_shift_does_not_include_multiple_shifted_letters(self):
+        phrases = gm.grammarize_phrase_shift('make fear')
+        self.assertNotIn('fake mare', phrases)
+
+    #
+    #
+    # full grammarian functionß
+
+    def test_grammarize_phrase_generates_phrases_from_substitution(self):
+        phrases = gm.grammarize_phrase('make fear')
+        self.assertIn('bake fear', phrases)
+        self.assertIn('make bear', phrases)
+        self.assertIn('make feat', phrases)
+        self.assertIn('made fear', phrases)
+
+    def test_grammarize_phrase_generates_phrases_from_deletion(self):
+        phrases = gm.grammarize_phrase('make fear')
+        self.assertIn('make far', phrases)
+        self.assertIn('make ear', phrases)
+
+    def test_grammarize_phrase_generates_phrases_from_shift(self):
+        phrases = gm.grammarize_phrase('make fear')
+        self.assertIn('make fare', phrases)
+
+    def test_grammarize_phrase_shift_does_not_include_original_phrase(self):
+        phrases = gm.grammarize_phrase('make fear')
+        self.assertNotIn('make fear', phrases)
+
+    def test_grammarize_phrase_substitute_does_not_include_multiple_words_with_substitutions(self):
+        phrases = gm.grammarize_phrase_substitute('make fear')
+        self.assertNotIn('bake bear', phrases)
+
+    def test_grammarize_word_delete_does_not_include_word_with_multiple_removals(self):
+        phrases = gm.grammarize_phrase_delete('fear fare')
+        self.assertNotIn('ear far', phrases)
+
+    def test_grammarize_phrase_shift_does_not_include_multiple_shifted_letters(self):
+        phrases = gm.grammarize_phrase('make fear')
+        self.assertNotIn('fake mare', phrases)
 
 
 
